bd80bb263312b3a3ec37b665c434e6f9b4b0039e
Shivam Saheb <gryfphen@gmail.com>
2019-03-28 17:36:19 -0400

    Modified Output of one function to return only the information I needed from the card. In this case that was specifically the name, id, and rarity if applicable

--- CPIdentifier.m
+++ CPIdentifier.m
@@ -2,7 +2,6 @@
 % CARD PRICE IDENTIFIER - American
 %%
 
-
 function [] = CPIdentifier()
     cardList = readtable("card_list.csv");
 
@@ -10,9 +9,9 @@ function [] = CPIdentifier()
         
 end
 
-function [url, cardId, rarity] = getTable(cardList)
+function [title, cardId, rarity] = getTable(cardList)
     
-    [rows] = size(cardList);
+    [rows] = height(cardList);
     
     for i=1:rows
         row = cardList(i,:);
@@ -26,65 +25,45 @@ function [url, cardId, rarity] = getTable(cardList)
         
         cardList.CurrentPricePerCard(i,1) = p;
         cardList.CurrentTotalPrice(i,1) = quantity*p;
-        
-        % The following 2 lines of code can be used as a check to ensure
-        % details in the csv file match details obtained directly from the
-        % url
-%         isValid = validateDetails(title, cardId, rarity);
-%         [title, isValid, rarity]
-
     end
     
     writetable(cardList, 'card_list_currents.csv');
 end
 
-function [isValid] = validateDetails(title, expectedCardId, expectedRarity)
-    titleSplit = strsplit(title, ' - ');
+% function [isValid] = validateDetails(title, expectedCardId, expectedRarity)
+%     titleSplit = strsplit(title, ' - ');
+% 
+%     actualName = string(titleSplit(1));
+%     actualCardId = string(titleSplit(2));
+%     actualRarity = string(titleSplit(3));    
+%     
+%     isValid = contains(actualCardId, expectedCardId) && contains(actualRarity, expectedRarity);
+% end
 
-    actualName = string(titleSplit(1));
-    actualCardId = string(titleSplit(2));
-    actualRarity = string(titleSplit(3));    
-    
-    isValid = contains(actualCardId, expectedCardId) && contains(actualRarity, expectedRarity);
-end
-
-function [title, p] = getDetails(url)
+function [name, p] = getDetails(url)
     tree = getHtmlTree(url);
-    title = getTitle(tree);
-    pri = getPrice(tree, url);
+    name = getName(tree);
+    pri = getPrice(tree);
     priSplit = strsplit(pri, '$');
     priSplit = priSplit{2};
     p = str2double(priSplit);
 end
 
 function [pageTree] = getHtmlTree(url)
-    % Find the URL, save it to a variable called "code"
     code = webread(url);
-
-    % Parse HTML code in "code"
     pageTree = htmlTree(code);
 end
 
-function [price] = getPrice(tree, url)
-    
-    % for the beta version of the website
-%     if  contains(url, "beta")
+function [price] = getPrice(tree)
         selector = "DIV.price";
         priceSection = findElement(tree, selector);
         price = extractHTMLText(priceSection);
-        price = price{2};
-        
-%     % for the old version    
-%     else
-%         selector = "SPAN.priceSection";
-%         priceSection = findElement(tree, selector);
-%         price = extractHTMLText(priceSection);
-%     end  
+        price = price{2}; 
 end
 
-function [name] = getTitle(tree)
-    selector = "DIV.itemHeader";
+function [name] = getName(tree)
+    selector = ".itemName";
     nameSection = findElement(tree, selector);
     name = extractHTMLText(nameSection);
+        
 end
-
